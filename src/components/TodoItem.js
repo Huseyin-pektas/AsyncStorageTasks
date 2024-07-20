aec3449d7c4cad1cedbed5ae7b067a374575502d
huseyin pektas <huseyinpektas@192.168.1.52>
2024-07-20 23:34:45 +0300

    secondary commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,87 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import colors from '../themes/Colors';
+import StatusButton from './StatusButton';
+import { useNavigation } from '@react-navigation/native';
+import ScreenName from '../constants/ScreenName';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const TodoItem = () => {
-  return (
-    <View>
-      <Text>TodoItem</Text>
-    </View>
-  )
+const TodoItem = ({ data }) => {
+    const navigation = useNavigation()
+
+    console.log(data);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.itemHeader}>
+
+                <Text style={[styles.taskList,
+                { textDecorationLine: data?.status === "closed" ? "line-through" : "null" }]
+                }>{data?.title.toUpperCase()}</Text>
+                <View style={{ flexDirection: "row" }}>
+                    <View style={styles.statusContainer}>
+                        <Text style={{
+                            color: data?.status === ("open" || "progress") ? "#72966f"
+                                : "#d6825c"
+                        }} >{data?.status} </Text>
+                    </View>
+                    <StatusButton iconName="pencil" color="green"
+                        onPress={() => navigation.navigate(ScreenName.addTask)}
+
+                    />
+                    <StatusButton iconName="delete" color="red" />
+                </View>
+            </View>
+            <View style={styles.footerContainer}>
+                <View>
+                    <Text>Başlangıç Tarihi</Text>
+                    <View style={{ flexDirection: "row", alignItems: "center" }}>
+                        <Icon name="clock-outline" size={15} />
+                        <Text
+                            style={{ marginLeft: 5, color: "purple", fontSize: 12, fontWeight: "700" }}>15.06.2024 - 15.59</Text>
+                    </View>
+                </View>
+                <View>
+                    <Text>Bitiş Tarihi</Text>
+                    <View style={{ flexDirection: "row", alignItems: "center" }}>
+                        <Icon name="clock-outline" size={15} />
+                        <Text style={{ marginLeft: 5, color: "purple", fontSize: 12, fontWeight: "700" }}>25.06.2024 - 15.59</Text>
+                    </View>
+                </View>
+            </View>
+        </View >
+    )
 }
 
 export default TodoItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.white,
+        padding: 15,
+        borderRadius: 15,
+        marginBottom: 15
+    },
+    itemHeader: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+
+    taskList: {
+        flex: 1,
+        fontSize: 15,
+        color: colors.text.primary,
+        fontWeight: "600",
+        marginBottom: 5
+    },
+    statusContainer: {
+        paddingHorizontal: 10,
+        paddingVertical: 5
+    },
+    footerContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    }
+})
--- src/pages/TaskListScreen.js
+++ src/pages/TaskListScreen.js
@@ -1,64 +1,69 @@
-import { Dimensions, SafeAreaView, StyleSheet, Text, View ,FlatList} from 'react-native'
+import { Dimensions, SafeAreaView, StyleSheet, Text, View, FlatList } from 'react-native'
 import React, { useState } from 'react'
 import colors from '../themes/Colors'
 import CustomTextInput from '../components/CustomTextInput'
 import TodoItem from '../components/TodoItem'
+import CustomButton from '../components/CustomButton'
+import { useNavigation } from '@react-navigation/native'
+import ScreenName from '../constants/ScreenName'
 
 const TaskListScreen = () => {
+  const navigation = useNavigation()
 
   const [search, setSearch] = useState("")
-  const [tasks , setTasks] = useState([
+  const [tasks, setTasks] = useState([
     {
-      userId:1,
-      id:1,
-      title:"title",
-      status:"closed"
+      userId: 1,
+      id: 1,
+      title: "title",
+      status: "closed"
     },
     {
-      userId:2,
-      id:2,
-      title:"title",
-      status:"open"
+      userId: 2,
+      id: 2,
+      title: "title",
+      status: "open"
     },
     {
-      userId:3,
-      id:3,
-      title:"title",
-      status:"done"
+      userId: 3,
+      id: 3,
+      title: "title",
+      status: "done"
     },
     {
-      userId:4,
-      id:4,
-      title:"title",
-      status:"closed"
+      userId: 4,
+      id: 4,
+      title: "title",
+      status: "closed"
     },
     {
-      userId:5,
-      id:5,
-      title:"title",
-      status:"closed"
+      userId: 5,
+      id: 5,
+      title: "title",
+      status: "closed"
     },
     {
-      userId:6,
-      id:6,
-      title:"title",
-      status:"closed"
+      userId: 6,
+      id: 6,
+      title: "title",
+      status: "closed"
     }
   ])
 
   return (
     <View style={styles.container}>
       <View style={styles.mainContainer}>
-        <SafeAreaView style={[styles.container,{marginBottom:20}]}>
-          <CustomTextInput value = ""
-          onChangeText={setSearch}
+        <SafeAreaView style={[styles.container, { marginBottom: 20 }]}>
+          <CustomTextInput value=""
+            onChangeText={setSearch}
           />
           <FlatList
-          data={tasks}
-          renderItem={({item})=> <TodoItem/>}
-          
+            data={tasks}
+            renderItem={({ item }) => <TodoItem data={item} />}
+
           />
         </SafeAreaView>
+        <CustomButton onPress={() => navigation.navigate(ScreenName.addTask)} label="Add Task" />
       </View>
     </View>
   )
@@ -71,10 +76,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.background.primary
   },
-  mainContainer:{
-    height:"100%",
-    position:"absolute",
-    width:Dimensions.get("screen").width,
-    padding:15
+  mainContainer: {
+    height: "100%",
+    position: "absolute",
+    width: Dimensions.get("screen").width,
+    padding: 15
   }
 })
